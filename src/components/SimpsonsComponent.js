0f3df02ab0ef2c141ffa6e9aaef55ea6a0e3150f
wintersleepmachine <46606053+wintersleepmachine@users.noreply.github.com>
2019-09-26 11:06:44 -0700

    Update SimpsonsComponent.js

--- src/components/SimpsonsComponent.js
+++ src/components/SimpsonsComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SimpsonsComponent = (props) => {
     return (
         <div>
-            <img src={props.image} alt=""/>
+            <img src={props.image} alt="" style = {{height: '80vh'}}/>
             <h2>"{props.quote}"</h2>
             <p style = {{fontSize: '30px'}}> - {props.character}</p>
         </div>
